99ab56f1924b0b36793347e05f695947c3c18b2f
Patricia Bueno <patibuenodev@gmail.com>
2025-10-10 10:33:24 -0300

    Update carousel.js

--- carousel.js
+++ carousel.js
@@ -1,36 +1,36 @@
 const slides = [
   {
-    img: "/img/Daniel.png",
+    img: "img/Daniel.png",
     name: "Daniel Magalhães",
     role: "Founder and Head of Strategy and Sales",
   },
   {
-    img: "/img/Edson.png",
+    img: "img/Edson.png",
     name: "Edson Lopes",
     role: "Co-founder and CEO",
   },
   {
-    img: "/img/Gabriela.png",
+    img: "img/Gabriela.png",
     name: "Gabriela Pini",
     role: "Head of Products",
   },
   {
-    img: "/img/Pantalena.png",
+    img: "img/Pantalena.png",
     name: "Alexandre Pantalena",
     role: "Head of Technology",
   },
   {
-    img: "/img/Josemar.png",
+    img: "img/Josemar.png",
     name: "Josemar Flausino",
     role: "Software developer",
   },
   {
-    img: "/img/Guilherme.png",
+    img: "img/Guilherme.png",
     name: "Guilherme Tavares",
     role: "Data engineer",
   },
   {
-    img: "/img/Patricia.png",
+    img: "img/Patricia.png",
     name: "Patricia Bueno",
     role: "Software developer",
   },
